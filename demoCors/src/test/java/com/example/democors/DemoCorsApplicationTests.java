8cf9ba5f273b7379ca574bafb4552121d19f2bd3
djarae <djaraestivill@gmail.com>
2024-06-09 21:53:13 -0400

    demoCors inicio

---
+++ demoCors/src/test/java/com/example/democors/DemoCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
